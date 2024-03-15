ba9391e62475c50dc3c1ececdc9c84bbb5815a4b
Gentry Atkinson <gatkinso@stedwards.edu>
2024-03-15 11:29:35 -0500

    figured out a linear fade for a short themesong

--- gen_music.py
+++ gen_music.py
@@ -7,18 +7,34 @@ from transformers import pipeline
 import numpy as np
 import scipy
 
-pipe = pipeline(
-    "text-to-audio",
-    model="facebook/musicgen-small",
-)
+def generate_theme_song():
+    pipe = pipeline(
+        "text-to-audio",
+        model="facebook/musicgen-small",
+    )
 
-output = pipe("A jazzy 10-second intro song for a science focused podcast")
+    output = pipe("An upbeat, jazzy intro song for a science focused podcast", forward_params={"do_sample": True})
 
-audio = output["audio"]
-#Channels Last
-audio = audio.squeeze()
-audio = np.moveaxis(audio, -1, 0)
-scipy.io.wavfile.write(
-        os.path.join("Podcast Generator", "tunes", f"themesong.wav"), 
-        rate=output['sampling_rate'], data=audio.astype(np.float32)
-)
+    audio = output["audio"]
+    #Channels Last
+    audio = audio.squeeze()
+    scipy.io.wavfile.write(
+            os.path.join("Podcast Generator", "tunes", "themesong.wav"), 
+            rate=output['sampling_rate'], data=audio.astype(np.float32)
+    )
+    # Shorten song to 6 seconds (assuming 30 second original length)
+    audio = audio[0:len(audio)//5]
+    # Linear fade out last two seconds
+    audio_length = len(audio)
+    fade_length = audio_length//3
+    fade_start = 2*audio_length//3
+    for i in range(fade_start, audio_length):
+        audio[i] *= (1 - ((i - fade_start)/fade_length))
+    np.save(os.path.join("Podcast Generator", "tunes", "shortened_themesong.npy"), audio)
+    scipy.io.wavfile.write(
+            os.path.join("Podcast Generator", "tunes", "shortened_themesong.wav"), 
+            rate=output['sampling_rate'], data=audio.astype(np.float32)
+    )
+
+if __name__ == "__main__":
+    generate_theme_song()
